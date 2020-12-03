2fc65c8b602a2f0f9d92c48f5f8673580aca8e3e
Ryan <raydude1999@hotmail.co.uk>
2020-12-03 13:37:31 +0000

    TestLevelUpdate
    Updated My test level with timed triggers and platforms

---
+++ Assets/Project/Scripts/DoorTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject door;
+
+    bool isOpened = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            door.transform.position += new Vector3(0, 4, 0);
+        }
+        else if (isOpened)
+        {
+            isOpened = false;
+            door.transform.position -= new Vector3(0, 4, 0);
+        }
+    }
+
+}
