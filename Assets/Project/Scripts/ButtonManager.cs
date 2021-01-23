811b77bd4e6ebd1f115acf547cf169ee37cf1c1f
Ryan <raydude1999@hotmail.co.uk>
2021-01-23 18:26:13 +0000

    MainMenu/Buttons Update
    Main menu with working buttons to r test level have been added and implemented.

---
+++ Assets/Project/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+     /*void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("RyanTestLevel", LoadSceneMode.Additive);
+    }  */
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene("RyanTestLevel", LoadSceneMode.Single);
+    }
+
+    public void ButtonCredits()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
