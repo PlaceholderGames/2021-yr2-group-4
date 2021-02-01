8035c32197154eaa512b3f393da997756058f48f
Ryan <raydude1999@hotmail.co.uk>
2021-02-01 23:13:40 +0000

    Menu's & Scene's
    Scene transitions added. test level semi-finished for previewing. Pause menu added.

---
+++ Assets/Project/Scenes/DamianTestLevel/TestEnemyScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestEnemyScript : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        animator.SetBool("IsWalking", false);
+        //If player in radius, chase 
+        if (distance <= lookRadius)
+        {
+            //Chase function
+            agent.SetDestination(target.position);
+            //If enemy is chasing player change animation from idle to walk
+            animator.SetBool("IsWalking", true);
+            //Look at player when close
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+                animator.SetBool("Attack", true);
+                Invoke("Restart", 2); //2 is the time
+                //Restart();
+            }
+        }
+        
+    }
+
+    //Look at player when close function
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
--- Assets/Project/Scripts/ButtonManager.cs
+++ Assets/Project/Scripts/ButtonManager.cs
@@ -1,25 +1,68 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ButtonManager : MonoBehaviour
 {
-     /*void Start()
+    public GameObject pauseMenu;
+    public bool isPaused;
+    public Animator transition;
+    public float transitionTime = 2;
+
+    void Update()
     {
-        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("RyanTestLevel", LoadSceneMode.Additive);
-    }  */
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
     public void ButtonStart()
     {
+        Time.timeScale = 1f;
+        //LoadNextLevel();
         SceneManager.LoadScene("RyanTestLevel", LoadSceneMode.Single);
     }
 
     public void ButtonCredits()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
     public void ButtonQuit()
     {
         Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(LevelIndex + 1);
     }
 }
