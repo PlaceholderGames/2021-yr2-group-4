0c7ab2db5829261cc2a6b70af5e8043416860cf3
Ryan <raydude1999@hotmail.co.uk>
2021-03-01 23:44:15 +0000

    New Level
    New scripts, level, fixed main character, most visual bugs fixed.

---
+++ Assets/Project/Scripts/MazeDataGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeDataGenerator
+{
+    public float placementThreshold;    // chance of empty space
+
+    //Dictates how many walls there are (Lower = morewalls)
+    public MazeDataGenerator()
+    {
+        placementThreshold = .1f;                               
+    }
+
+    public int[,] FromDimensions(int sizeRows, int sizeCols)    // 2
+    {
+        int[,] maze = new int[sizeRows, sizeCols];
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                //Maze size (I believe)
+                if (i == 0 || j == 0 || i == rMax || j == cMax)
+                {
+                    maze[i, j] = 1;
+                }
+
+                
+                
+
+               //Inner walls spawning controller
+               else if (i % 2 == 0 && j % 2 == 0)
+                {
+                    if (Random.value > placementThreshold)
+                    {
+                        //anything other than 1 turns walls into isolated squares
+                        maze[i, j] = 1;
+
+                        int a = Random.value < .5 ? 0 : (Random.value < .5 ? -1 : 1);
+                        int b = a != 0 ? 0 : (Random.value < .5 ? -1 : 1);
+                        maze[i + a, j + b] = 1;
+                    }
+                }
+           }
+        }
+
+        return maze;
+    }
+}
