70d1c1a193f799d40c5540d8cc23c12b35c3bd02
17078601 <44087137+17078601@users.noreply.github.com>
2021-04-22 22:35:16 +0100

    press z - Audio visualiser for accessibility

---
+++ Assets/VisualSound.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualSound : MonoBehaviour
+{
+    AudioSource[] sources;
+    Text text;
+    GameObject AudioCue;
+
+    void Start()
+    {
+
+        text = FindObjectOfType<Text>();
+
+        sources = GameObject.FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+
+        AudioCue = text.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            AudioCue.SetActive(!AudioCue.activeInHierarchy);
+        }
+
+            // When a key is pressed list all the gameobjects that are playing an audio
+            text.text = null;
+        foreach (AudioSource audioSource in sources)
+        {
+            if (audioSource.isPlaying) { text.text = text.text + audioSource.clip.name + "\n"; }
+        }
+
+
+     
+    }
+}
+
